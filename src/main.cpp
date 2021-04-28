413a643d3ed6c8860ea78fa5ef1add76f06742ae
Mauricio Castro (chromebook) <mauricio.cruells@gmail.com>
2021-04-27 22:29:09 -0600

    main.cpp update after button library creation

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,3 @@ void setup() {
 void loop() {
 
 }
-
-
-
-class Button{
-  
-  volatile bool state;
-  int pin;
-  String contactor;
-  int interrupt;
-
-  public:
-  Button(int pinNumber, String contactorOperation, int interruptNumber){
-    pin = pinNumber;
-    pinMode(pin, INPUT_PULLUP);
-
-    contactor = contactorOperation;
-    interrupt = interruptNumber;
-  }
-
-  void set(){
-    state = true;
-  }
-
-  void reset(){
-    state = false;
-  }
-
-  bool getState(){
-    return state;
-  }
-
-};
