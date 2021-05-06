7307903578c0ff3efead70a1f2ca92f1ee3881fd
Mauricio Castro (chromebook) <mauricio.cruells@gmail.com>
2021-05-06 11:49:45 -0600

    implemented manual and automatic operation

--- src/main.ino
+++ src/main.ino
@@ -2,37 +2,63 @@
 #include "Button.h"
 
 bool runFlag = false;
+bool autoFlag = false;
+bool manualFlag = false;
 
-Button green(2, "NO", 0);
+int autoManPosA = 9;
+int autoManPosB = 10;
+
+//Button init, pin number, contactor logic, interrupt to attach
+Button green(2, "NO", 0); 
 Button red(3, "NC", 1);
+
 
 void setup() {
     //debugging
-
-    Serial.begin(9600);
-
+    //Serial.begin(9600);
     //end debugging
+    
     attachInterrupt(green.getInterrupt(), updateGreenFlag, green.getLogic()); 
     attachInterrupt(red.getInterrupt(), updateRedFlag, red.getLogic());
     
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(autoManPosA, INPUT_PULLUP);
+    pinMode(autoManPosB, INPUT_PULLUP);
 }
 
 void loop() {
 
     //debugging
-    Serial.println(String(runFlag) + " " + String(green.getState()) + " " + String(red.getState()));
+    //Serial.println(String(runFlag) + " " + String(green.getState()) + " " + String(red.getState()));
     //end debugging
-    
-    if(runFlag){
-        digitalWrite(LED_BUILTIN, HIGH);
-    }
-    else if (!runFlag){
-        digitalWrite(LED_BUILTIN, LOW);
+    manStateUpdate(); // rutina para ver en que posicion esta la maneta de automatico y manual
+
+    if(manualFlag){
+        if(runFlag){
+            digitalWrite(LED_BUILTIN, HIGH);
+        }
+        else if (!runFlag){
+            digitalWrite(LED_BUILTIN, LOW);
+        }
     }
 
 }
 
+
+void manStateUpdate(){
+    if((digitalRead(autoManPosA) == false) & (digitalRead(autoManPosB) == true)){
+        autoFlag = true;
+        manualFlag = false;
+    }
+    else if((digitalRead(autoManPosB) == false) & (digitalRead(autoManPosA) == true)){
+        autoFlag = false;
+        manualFlag = true;
+    }
+    else{
+        autoFlag = false;
+        manualFlag = false;
+    }
+}
 
 //ISRs for button operation
 
@@ -47,3 +73,4 @@ void updateRedFlag(){
     red.setState();
     runFlag = green.getState();
 }
+
